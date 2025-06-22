a3160a90888f16fb16bb97af56d4bd93fc10607d
Carlos Lucena Robles <carlos.lucena022@gmail.com>
2025-06-22 12:03:13 +0200

    Added frame documentation

--- include/udp_handler.h
+++ include/udp_handler.h
@@ -3,6 +3,32 @@
  * @brief Declaraciones para el manejo de peticiones UDP.
  * @author Carlos Lucena Robles
  * @date 2025-06-21
+ *
+ * @section formato_trama_entrada Formato de trama de entrada (UDP)
+ *
+ * La trama recibida debe tener el siguiente formato (mínimo 35 bytes):
+ *
+ * | Offset | Tamaño | Campo              | Descripción                       |
+ * |--------|--------|--------------------|-----------------------------------|
+ * | 0      | 31     | version            | Versión de firmware (string)      |
+ * | 32     | 2      | sensorId           | ID del sensor (uint16_t)          |
+ * | 34     | 1      | statusCode         | Código de estado (uint8_t)        |
+ *
+ * @section formato_trama_salida Formato de trama de salida (UDP)
+ *
+ * La respuesta enviada tiene el siguiente formato (55 bytes):
+ *
+ * | Offset | Tamaño | Campo              | Descripción                       |
+ * |--------|--------|--------------------|-----------------------------------|
+ * | 0      | 31     | firmwareVersion    | Versión de firmware (string)      |
+ * | 32     | 1      | frequency          | Frecuencia (uint8_t)              |
+ * | 33     | 1      | scale              | Escala (uint8_t)                  |
+ * | 34     | 1      | enable             | Habilitado (uint8_t)              |
+ * | 35     | 4      | thresholdStdv      | Umbral (float)                    |
+ * | 39     | 8      | secs               | Timestamp (segundos, uint64_t)    |
+ * | 47     | 8      | usecs              | Timestamp (microsegundos, uint64_t)|
+ *
+ * Ver detalles y uso en udp_handler.c
  */
 
 #ifndef UDP_HANDLER_H
