e5ab3d02e36da8bd1e429fc5a6505881cc541e98
Carlos Lucena Robles <carlos.lucena022@gmail.com>
2025-06-22 12:03:34 +0200

    Updated docs

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['enable_0',['enable',['../d3/df6/structSensorConfig.html#ae59b3dd1069c9db41bebd50de120f628',1,'SensorConfig']]]
+  ['enable_0',['enable',['../d3/df6/structSensorConfig.html#ae59b3dd1069c9db41bebd50de120f628',1,'SensorConfig']]],
+  ['entrada_20udp_1',['Formato de trama de entrada (UDP)',['../d7/d08/udp__handler_8h.html#formato_trama_entrada',1,'']]]
 ];
--- docs/html/search/all_b.js
+++ docs/html/search/all_b.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['tag_5fudp_0',['TAG_UDP',['../d5/d60/utils_8h.html#a434efbe20b1838cc4dbb4fd13d55d4a2',1,'utils.h']]],
-  ['thresholdstdv_1',['thresholdStdv',['../d3/df6/structSensorConfig.html#a780ce14ae4e53d3dc331d2f84e603dca',1,'SensorConfig']]]
+  ['thresholdstdv_1',['thresholdStdv',['../d3/df6/structSensorConfig.html#a780ce14ae4e53d3dc331d2f84e603dca',1,'SensorConfig']]],
+  ['trama_20de_20entrada_20udp_2',['Formato de trama de entrada (UDP)',['../d7/d08/udp__handler_8h.html#formato_trama_entrada',1,'']]],
+  ['trama_20de_20salida_20udp_3',['Formato de trama de salida (UDP)',['../d7/d08/udp__handler_8h.html#formato_trama_salida',1,'']]]
 ];
